3c3c2298e663b8c2ecd851373748cbf1ddf489f9
mhdsafvan15-blip <mhdsafvan15@gmail.com>
2025-11-17 11:38:28 +0530

    Time: 29 ms (1.64%), Space: 150.1 MB (42.62%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.dart
@@ -0,0 +1,8 @@
+class Solution {
+  bool isSameTree(dynamic p, dynamic q) {
+    if (p == null && q == null) return true;
+    if (p == null || q == null) return false;
+    if (p.val != q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+}
